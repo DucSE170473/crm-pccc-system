6f1626aaaa9888bd72da520cc50e139c0ea1779a
DucSE170473 <ducha.bidv@gmail.com>
2026-03-27 10:04:29 +0700

    init: create spring boot project

---
+++ src/main/java/com/thanhcong/crm/PcccCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thanhcong.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcccCrmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcccCrmBackendApplication.class, args);
+    }
+
+}
